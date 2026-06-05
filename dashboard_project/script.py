c31521d086ef608584f3bb408aee64b1278384f6
hudatalat719-oss <hudatalat719@gmail.com>
2026-06-05 19:25:13 +0500

    Delete dashboard_project/script.py

--- dashboard_project/script.py
+++
@@ -1,3 +0,0 @@
-mkdir -p /home/claude/dashboard_project/data /home/claude/dashboard_project/notebooks
-cp /mnt/user-data/uploads/1976-2024-president__1_.csv /home/claude/dashboard_project/data/
-ls /home/claude/dashboard_project/data/
